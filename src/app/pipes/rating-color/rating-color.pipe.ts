fec316ca5341932bdfab4a7aac27028e7fccf5c5
Vojtech Mašek <vojtech.masek@flowup.cz>
2020-05-17 20:58:40 +0200

    feat(query-params-demo): meta rating no data handling

--- src/app/pipes/rating-color/rating-color.pipe.ts
+++ src/app/pipes/rating-color/rating-color.pipe.ts
@@ -5,6 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class RatingColorPipe implements PipeTransform {
   transform(rawValue: number): string {
+    if (!rawValue) {
+      return 'hsl(0, 0%, 60%)';
+    }
+
     const value = rawValue > 1 ? rawValue / 100 : rawValue;
     const hue = (value * 120).toString(10);
     return ['hsl(', hue, ',100%,50%)'].join('');
